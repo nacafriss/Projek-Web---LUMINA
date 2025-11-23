02bf83676da943a3d726abbd4f8e74ca1792d806
chostanz <dewianyar21@gmail.com>
2025-11-23 20:13:46 +0700

    add

--- index.php
+++ index.php
@@ -1,28 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-         <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/style.css">
 
 </head>
+
 <body>
-    <!--Font Awesome CDN-->
-
     <div class="container">
-   <header class="header">
-        <nav class="navbar">
-        <a href="index.html">Home</a>
-        <a href="about.html">Destination</a>
-        <a href="contact.html">About</a>
-        <a href="weather.html">Contact</a>
-      </nav>
-    </header>
+        <header class="header">
+            <nav class="navbar">
+                <a href="index.html">Home</a>
+                <a href="about.html">Destination</a>
+                <a href="contact.html">About</a>
+                <a href="weather.html">Contact</a>
+            </nav>
+        </header>
         <div class="slide">
-
-            
             <div class="item" style="background-image: url(https://plus.unsplash.com/premium_photo-1668883189682-7212bebf2f5b?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D);">
                 <div class="content">
                     <div class="name">Switzerland</div>
@@ -76,4 +74,5 @@
     </div>
     <script src="main.js"></script>
 </body>
+
 </html>
