d72a5797ab1ff9b293ea27eb66b26113f5e6993b
chostanz <dewianyar21@gmail.com>
2025-12-03 10:50:11 +0700

    fix

--- logic/delete.logic.php
+++ logic/delete.logic.php
@@ -3,7 +3,6 @@ include "../config/koneksi.php";
 
 $id = $_GET['id'];
 
-// hapus booking user dulu biar FK aman
 mysqli_query($koneksi, "DELETE FROM destinations WHERE =$id");
 
 header("Location: ../dashboard.php");
