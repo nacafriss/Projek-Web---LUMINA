f2f5ce0dc9718e1201996e793cb5b0d31e4621f9
chostanz <dewianyar21@gmail.com>
2025-12-03 10:51:39 +0700

    fix

--- logic/delete.logic.php
+++ logic/delete.logic.php
@@ -3,7 +3,7 @@ include "../config/koneksi.php";
 
 $id = $_GET['id'];
 
-mysqli_query($koneksi, "DELETE FROM destinations WHERE =$id");
+mysqli_query($koneksi, "DELETE FROM destinations WHERE id=$id");
 
 header("Location: ../dashboard.php");
 
